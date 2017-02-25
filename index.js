4e18de66f4cd6b6ad9ea0ac53f7d7fc51c4c0f9d
FowiFlowi <fowiflowi@gmail.com>
2017-02-25 17:10:48 +0200

    fixed sunday

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api'),
 	express = require('express'),
 	bodyParser = require('body-parser'),
+	fs = require('fs'),
 
 	Abstracts = require('./models/abstract'),
 	SerdechkoBot = require('./models/telegram-bot'),
